ac259dd6b461a90e10dc3dae4029784b87115a7d
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-03-30 19:57:02 +0300

    удалил какие-то символы

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import Menu from "./components/Menu/Menu.jsx";
 function HomePage() {
   return (
     <article className="article">
-      <h1 className="article__title">Гоночн​ое такси</h1>
+      <h1 className="article__title">Гоночное такси</h1>
       <p className="article__paragraph">
-        Гоночн​ое такси – отличная возможность насладиться скоростью и мастерством гонщика,
+        Гоночное такси – отличная возможность насладиться скоростью и мастерством гонщика,
         сидя на месте штурмана, и стать свидетелем настоящего мастерства профессиональных
         инструкторов Сочи Автодрома, в полной мере ощутив крутые виражи на самой современной
         гоночной трассе России.
@@ -23,7 +23,7 @@ function DriftPage() {
     <article className="article">
       <h1 className="article__title">Дрифт-такси</h1>
       <p className="article__paragraph">
-        Только ​на Сочи Автодроме вас ждет уникальная возможность
+        Только на Сочи Автодроме вас ждет уникальная возможность
         промчаться по трассе Формулы 1 на максимально возможной
         скорости в управляемом заносе на легендарной «королеве дрифта» Nissan Silvia!
       </p>
@@ -57,7 +57,7 @@ function ForzaPage() {
 function TimeAttackPage() {
   return (
     <article className="article">
-      <h1 className="article__title">Гонка ​​​​​​Time Attack</h1>
+      <h1 className="article__title">Гонка Time Attack</h1>
       <p className="article__paragraph">
         Гонка Time Attack создана для тех, кто любит ставить рекорды –
         открытое соревнование на гражданских автомобилях не предполагает
